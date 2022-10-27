4281bea82f32ab76496ec9547c006577f8dcedaf
zxvzxv <41340914+zxvzxv@users.noreply.github.com>
2022-10-27 16:22:56 +0800

    同步代码

---
+++ benchmark.py
@@ -0,0 +1,60 @@
+import os,sys
+from cProfile import Profile
+import datetime
+from types import FunctionType
+
+SELF_PATH = os.path.dirname(os.path.realpath(__file__))
+from parser.zip_parser import ZipFile
+from main import ApkFile
+from androguard.core.bytecodes.apk import APK
+
+test_apk = os.path.join(SELF_PATH, "test/apks/app-debug.apk")
+
+# 与androguard库简单对比，确保不要有很大的性能差距
+
+def timer(func:FunctionType):
+    '''
+    获取函数运行时间，测试用
+    '''
+    profile = Profile()
+
+    def get_time_and_run(*args, **kwargs):
+        profile.enable()
+        result = func(*args, **kwargs)
+        profile.disable()
+        
+        print(f'-------func name: {func.__name__}--------')
+        profile.print_stats("tottime")
+        return result
+
+    return get_time_and_run
+
+def get_file():
+    a = APK(sys.argv[1])
+    print(a.get_package())
+    # zip_file = ZipFile(sys.argv[1])
+    # for k,v in zip_file.cds.items():
+    #     print(k)
+
+    # for f in [b'classes.dex',b'classes2.dex',b'classes3.dex',b'classes4.dex']:
+    #     res = zip_file.get_file(f)
+    #     with open(f'/mnt/c/Users/user/Downloads/{f.decode()}', 'wb') as fw:
+    #         fw.write(res)
+    # assert len(res) == 22528
+
+
+@timer
+def arsc(target):
+    if target == 1:
+        a = APK(test_apk)
+        pkg = a.get_package()
+        res = a.get_android_resources().get_color_resources(pkg)
+    else:
+        a = ApkFile(test_apk)
+        res = a.get_resources(0x7F050021)
+
+    print(target, res)
+
+if __name__ == "__main__":
+    # arsc(1)
+    arsc(2)
---
+++ test/axml_test.py
@@ -0,0 +1,30 @@
+import os,sys
+
+SELF_PATH = os.path.dirname(os.path.realpath(__file__))
+ROOT_PATH = os.path.join(SELF_PATH, "../")
+sys.path.append(ROOT_PATH)
+from parser.zip_parser import ZipFile
+from parser.axml_parser import Axml, Arsc
+
+def test_axml_basic():
+    zip_file = ZipFile(os.path.join(SELF_PATH ,"apks/normal.apk"))
+    zip_file2 = ZipFile(os.path.join(SELF_PATH ,"apks/arsc_obf.apk"))
+
+    axml_1 = zip_file.get_file(b"AndroidManifest.xml")
+    axml_1 = Axml(axml_1)
+
+    axml_2 = zip_file2.get_file(b"AndroidManifest.xml")
+    axml_2 = Axml(axml_2)
+
+    string_encoding = axml_1.string_pool.is_utf8
+    print(axml_2.get_xml_str())
+    
+
+def test_arsc_basic():
+    with open(os.path.join(SELF_PATH ,"apks/resources.arsc"),"rb") as fr:
+        arsc = Arsc(fr.read())
+        arsc.get_resources(0x7f010000)
+
+
+if __name__ == "__main__":
+    test_arsc_basic()
