d1d1d35cbcdf53384a9f9222c1a82f9025b4a781
zxvzxv <41340914+zxvzxv@users.noreply.github.com>
2023-08-14 16:09:24 +0800

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='ApkParse',
       author='zxv',
       author_email='751269951@qq.com',
       requires= ['lxml'], # 定义依赖哪些模块
-      packages=["ApkParse"],  # 系统自动从当前目录开始找包
+      packages=find_packages(),  # 系统自动从当前目录开始找包
       # 如果有的文件不用打包，则只能指定需要打包的文件
       license="apache 3.0"
       )
